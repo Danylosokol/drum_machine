717dae1e60a27df08362314c35ac35f20ae6e9c6
dsoSU <dso@scaleup.agency>
2022-07-21 16:47:57 +0200

    add sound on button click

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import sound1 from "./kick1.mp3";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    
+
+    this.play = this.play.bind(this);
+    this.handleKey = this.handleKey.bind(this);
+  }
+  handleKey(event){
+    console.log("test");
+    console.log(event.key);
+  }
+
+  componentWillMount: function() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  },
+
+  componentWillUnmount: function() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  },
+
+  play(event){
+    const audio = event.target.querySelector("audio");
+    audio.play();
+  }
+
+  render(){
+    return (
+      <div id="drum-machine" tabIndex={0} onKeyDown={this.handleKey}>
+        <div class="grid-container">
+          <div className="drum-pad">Q</div>
+          <div onClick={this.play} className="drum-pad">
+            <audio src="./kick1.mp3" crossOrigin="anonymous"></audio>W
+          </div>
+          <div className="drum-pad">E</div>
+
+          <div className="drum-pad">A</div>
+          <div className="drum-pad">S</div>
+          <div className="drum-pad">D</div>
+
+          <div className="drum-pad">Z</div>
+          <div className="drum-pad">X</div>
+          <div className="drum-pad">C</div>
+        </div>
+        <div>
+          <div id="display"></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
